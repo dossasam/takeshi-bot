6574e33b9518fbee6ac67c71455edf9f04b36dbb
dossasam <newsam052@gmail.com>
2025-11-10 17:29:21 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -17,7 +17,7 @@ exports.BOT_NUMBER = "558112345678";
 // Número do dono bot.
 // Apenas números, exatamente como está no WhatsApp.
 // Se o seu número não exibir o nono dígito (9) no WhatsApp, não coloque-o.
-exports.OWNER_NUMBER = "5521950502020";
+exports.OWNER_NUMBER = "22891153473";
 
 // LID do dono do bot.
 // Para obter o LID do dono do bot, use o comando <prefixo>get-lid @marca ou +telefone do dono.
